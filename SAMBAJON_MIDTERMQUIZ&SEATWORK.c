e74d383b79875c5b614ff13212193ce3c5684ddd
marielsambajon <2025-1-00296@lpu.edu.ph>
2025-10-02 12:07:56 +0800

    Add files via upload

---
+++ SAMBAJON_MIDTERMQUIZ&SEATWORK.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+    int main(){
+        int i;
+        
+        for(i=1; i <= 100; i++){
+            printf("Mariel Francinne V. Sambajon:\n");
+        }
+        for(i = 150; i>=0; i--){
+            printf("%d\n", i);
+        }
+        
+        return 0;
+    }
